5f1d3e70b96592bd13959fd8e853f23472a25a91
Issaka Faisal <faisallarai@gmail.com>
2017-10-04 00:16:58 -1100

    add environment folder

--- environments/prod/db.php
+++ environments/prod/db.php
@@ -1,18 +1,26 @@
 <?php
+$host = $username = $password = $dbname = '';
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+if (isset($url["host"]) && isset($url["user"]) && isset($url["pass"]) && isset($url["path"])) {
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+}
 
 return [
     'class' => 'yii\db\Connection',
+    //'dsn' => 'mysql:host=' . $host . ';dbname=' . $dbname,
     //'dsn' => 'mysql:host=localhost;dbname=inventory_db', // MySQL, MariaDB
     //'dsn' => 'sqlite:/path/to/database/file', // SQLite
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=inventory_db', // PostgreSQL
+    'dsn' => 'pgsql:host=' . $host . ';port=5432;dbname=' . $dbname, // PostgreSQL
    //'dsn' => 'cubrid:dbname=demodb;host=localhost;port=33000', // CUBRID
    //'dsn' => 'sqlsrv:Server=localhost;Database=mydatabase', // MS SQL Server, sqlsrv driver
    //'dsn' => 'dblib:host=localhost;dbname=mydatabase', // MS SQL Server, dblib driver
    //'dsn' => 'mssql:host=localhost;dbname=mydatabase', // MS SQL Server, mssql driver
    //'dsn' => 'oci:dbname=//localhost:1521/mydatabase', // Oracle
-    'username' => 'postgres',
-    'password' => 'postgres',
+    
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 ];
-
-
